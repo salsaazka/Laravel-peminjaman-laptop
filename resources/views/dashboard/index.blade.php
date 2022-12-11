e8da4fcdaffcb879fb0aebed7b3c302bf37dd406
Yogapratama1109 <yoga110011@gmail.com>
2022-12-11 11:22:20 +0700

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,18 +1,70 @@
 @extends('layout')
 
 @section('content')
+    <center>
+        <div class="container">
+            <div class="card w-75" style="margin-top: 3rem; margin-bottom: 2rem">
+                <div class="p-4">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <h6 class="text-muted mb-1">Lab RPL/PPLG</h6>
+                            <h6 class="text-muted">Laptop Landing</h6>
+                        </div>
+                        <div>
+                            <img src="{{ asset('assets/img/wikrama.jpeg') }}">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center" style="margin-top: -1rem">
+                        <div class="d-flex flex-column justify-content-center align-items-center">
+                            <img src="{{ asset('assets/img/rpl.png') }}" style="width: 110px; height: 110px">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <div class="card card2">
+                                <div class="card-body">
+                                    <center class="mt-3">
+                                        <img src="{{ asset('assets/img/data.png') }}" style="width: 250px; height: 100%">
+                                        <br>
+                                        <h3 class="mt-4 fw-semibold text-primary">Table Data</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, at. Fugit, eveniet impedit culpa eum architecto saepe modi eaque provident tempora necessitatibus deleniti veritatis repellendus sequi reprehenderit! Possimus, soluta perspiciatis.</p>
+                                        <a href="{{ route('data') }}" class="btn btn-primary mt-2 mb-2">View Data</a>
+                                    </center>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card card2">
+                                <div class="card-body">
+                                    <center class="mt-3">
+                                        <img src="{{ asset('assets/img/form.png') }}" style="width: 250px; height: 100%">
+                                        <br>
+                                        <h3 class="mt-4 fw-semibold text-primary">Input Data</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, at. Fugit, eveniet impedit culpa eum architecto saepe modi eaque provident tempora necessitatibus deleniti veritatis repellendus sequi reprehenderit! Possimus, soluta perspiciatis.</p>
+                                        <a href="{{ route('create') }}" class="btn btn-primary mt-2 mb-2">Input Data</a>
+                                    </center>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </center>
 
-<div class="container d-flex justify-content-center mt-5 pt-5">
+    {{-- <div class="container d-flex justify-content-center mt-5 pt-5">
 
   <div class="card p-3 card-1">
-   
+
       <div class="info d-flex justify-content-between align-items-center">
 
           <div class="group d-flex flex-column">
 
               <span class="font-weight-bold"><img src="{{ asset('assets/img/wikrama.jpeg') }}" style="width: 80"> Data Peminjaman Laptop
               </span>
-              {{-- <small><i class="fa-solid fa-right-left"></i>{{ !is_null($borrow) ? count($borrow) : '-' }} Total laptop returned thid day</small> --}}
+
+              Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
+
 
           </div>
 
@@ -57,5 +109,5 @@
   </div>
 
 
-</div>
-  @endsection
+</div> --}}
+@endsection
